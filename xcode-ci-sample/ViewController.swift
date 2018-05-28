d67823e9de6de5a7b907eb58131a1e8149ebc1c7
Tomoyuki Ito <ito@nagisa-inc.jp>
2018-05-28 12:05:14 +0900

    feat: first commit

---
+++ xcode-ci-sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  xcode-ci-sample
+//
+//  Created by Tomoyuki Ito on 2018/05/28.
+//  Copyright © 2018 Nagisa Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
